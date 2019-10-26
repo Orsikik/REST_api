e53f16d1808619e6688e5b9817e080dbadcc59de
orsikik <orsikik@gmail.com>
2019-10-26 18:32:32 +0300

    User manager model created

--- REST_api/settings.py
+++ REST_api/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'REST_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rest_api',
+        'USER': 'db_admin',
+        'PASSWORD': 'pepsicola',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -121,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
